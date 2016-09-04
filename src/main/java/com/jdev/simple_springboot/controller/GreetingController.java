2c39fa0eff4d512f87537f5102f8488481160127
Joel Guilarte <joel@tailormade.systems>
2016-09-04 10:33:30 -0400

    Adding Simple Springboot REST Project

---
+++ src/main/java/com/jdev/simple_springboot/controller/GreetingController.java
@@ -0,0 +1,24 @@
+package com.jdev.simple_springboot.controller;
+
+import com.jdev.simple_springboot.model.GreetingResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by joel
+ */
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public GreetingResponse greeting(@RequestParam(value="name", defaultValue="World") String name) {
+
+        return new GreetingResponse(counter.incrementAndGet(), String.format(template, name));
+    }
+}
---
+++ src/main/java/com/jdev/simple_springboot/model/GreetingResponse.java
@@ -0,0 +1,26 @@
+package com.jdev.simple_springboot.model;
+
+/**
+ * Created by joel.
+ */
+public class GreetingResponse {
+
+    private final long id;
+    private final String content;
+
+    public GreetingResponse(long id, String content) {
+
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+
+        return id;
+    }
+
+    public String getContent() {
+
+        return content;
+    }
+}
