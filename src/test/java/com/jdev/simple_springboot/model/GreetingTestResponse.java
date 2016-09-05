2585842a8b103ded7ebb9760795a20716adec7a6
Joel Guilarte <jguilarte01@gmail.com>
2016-09-05 10:04:26 -0400

    Added integration test to make a rest connection to the spring boot app and retrieve a greeting response.

---
+++ src/test/java/com/jdev/simple_springboot/model/GreetingTestResponse.java
@@ -0,0 +1,37 @@
+package com.jdev.simple_springboot.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * Created by joel.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GreetingTestResponse {
+
+    private long id;
+    private String content;
+
+    public GreetingTestResponse() {
+
+    }
+
+    public long getId() {
+
+        return id;
+    }
+
+    public void setId(long id) {
+
+        this.id = id;
+    }
+
+    public String getContent() {
+
+        return content;
+    }
+
+    public void setContent(String content) {
+
+        this.content = content;
+    }
+}
